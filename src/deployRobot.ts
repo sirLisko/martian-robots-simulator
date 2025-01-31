78b3f4562ae64f550453c9a522b9474fe7990e60
Luca Lischetti <luca@sirlisko.com>
2025-01-31 12:36:32 +0000

    refactor: improved code quality and readability

--- src/deployRobot.ts
+++ src/deployRobot.ts
@@ -1,39 +1,56 @@
 const GRID = "5 3";
+const GRID_X = parseInt(GRID.split(" ")[0], 10);
+const GRID_Y = parseInt(GRID.split(" ")[1], 10);
 
 const DIRECTIONS = ["N", "E", "S", "W"] as const;
 type Direction = (typeof DIRECTIONS)[number];
 
+const calculateNextPosition = (
+  x: number,
+  y: number,
+  directionIndex: number,
+): [number, number] => {
+  switch (DIRECTIONS[directionIndex]) {
+    case "N":
+      return [x, y + 1];
+    case "E":
+      return [x + 1, y];
+    case "S":
+      return [x, y - 1];
+    case "W":
+      return [x - 1, y];
+    default:
+      throw new Error("Invalid direction");
+  }
+};
+
+const isOutOfBounds = (x: number, y: number): boolean =>
+  x < 0 || y < 0 || x >= GRID_X || y >= GRID_Y;
+
 export const deployRobot = (position: string, movements: string): string => {
   const [x, y, direction] = position.split(" ");
-  const [gridX, gridY] = GRID.split(" ").map(Number);
-  let [currentX, currentY] = [Number(x), Number(y)];
+  let [currentX, currentY] = [parseInt(x, 10), parseInt(y, 10)];
   let currentDirection = DIRECTIONS.indexOf(direction as Direction);
 
-  for (let i = 0; i < movements.length; i++) {
-    if (movements[i] === "R") {
+  for (const movement of movements) {
+    if (movement === "R") {
       currentDirection = (currentDirection + 1) % DIRECTIONS.length;
-    } else if (movements[i] === "L") {
-      currentDirection = (currentDirection + 3) % DIRECTIONS.length;
-    } else if (movements[i] === "F") {
-      if (currentDirection === 0) {
-        currentY++;
-      } else if (currentDirection === 1) {
-        currentX++;
-      } else if (currentDirection === 2) {
-        currentY--;
-      } else if (currentDirection === 3) {
-        currentX--;
+    } else if (movement === "L") {
+      currentDirection =
+        (currentDirection + DIRECTIONS.length - 1) % DIRECTIONS.length;
+    } else if (movement === "F") {
+      const [nextX, nextY] = calculateNextPosition(
+        currentX,
+        currentY,
+        currentDirection,
+      );
+
+      if (isOutOfBounds(nextX, nextY)) {
+        return `${nextX} ${nextY} ${DIRECTIONS[currentDirection]} LOST`;
+      } else {
+        [currentX, currentY] = [nextX, nextY];
       }
     }
-    if (
-      currentX >= gridX ||
-      currentY >= gridY ||
-      currentX < 0 ||
-      currentY < 0
-    ) {
-      return `${currentX} ${currentY} ${direction} LOST`;
-    }
   }
-
   return `${currentX} ${currentY} ${DIRECTIONS[currentDirection]}`;
 };
