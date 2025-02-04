424dd1ff05c79c01dfb77bbfb5e6e09c942619d5
Luca Lischetti <luca@sirlisko.com>
2025-02-04 14:32:33 +0000

    docs: added comment for logic on the constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const DIRECTIONS = ["N", "E", "S", "W"] as const;
-export type Direction = (typeof DIRECTIONS)[number];
 export const MAX_COORDINATE = 50;
 export const MAX_COMMANDS = 100;
+export const DIRECTIONS = ["N", "E", "S", "W"] as const; // they must be defined in a clockwise direction.
+export type Direction = (typeof DIRECTIONS)[number];
